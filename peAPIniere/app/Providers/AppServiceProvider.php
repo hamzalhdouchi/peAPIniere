142e9a3df27b0ab3d0972213095b334ad2dffb8e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-19 20:49:09 +0000

    ajouter les bind pour les rolation entre les plant interface et plants repository

--- peAPIniere/app/Providers/AppServiceProvider.php
+++ peAPIniere/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\PlanteRepository;
+use App\Repositories\PlanteRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PlanteRepositoryInterface::class, PlanteRepository::class);
     }
 
     /**
