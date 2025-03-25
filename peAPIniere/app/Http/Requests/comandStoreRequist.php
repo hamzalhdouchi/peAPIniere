f8267c7c0dae5b8d8b12e6c953f51d94b4c28d3e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-25 21:25:38 +0000

    add les validation request

--- peAPIniere/app/Http/Requests/comandStoreRequist.php
+++ peAPIniere/app/Http/Requests/comandStoreRequist.php
@@ -11,7 +11,7 @@ class comandStoreRequist extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class comandStoreRequist extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'required|exists:users,id',
+    'plante_id' => 'required|exists:plantes,id',
+    'quantity' => 'required|integer|min:1',
+    'acciptaion' => 'sometimes|in:accepte,refuser',
+    'statut' => 'sometimes|in:pending,in_preparation,delivered'
         ];
     }
 }
